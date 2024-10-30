854f18d07426b284e09b84a81d5e1aad7155b464
André <145787269+Andreheinzen@users.noreply.github.com>
2024-10-30 09:32:50 -0300

    30/10

--- aap.js
+++ aap.js
@@ -1,18 +1,18 @@
-function criaCartao(categoria, pergunta, resposta){ 
-let container = document.getElementById('container');
-let cartao = document.createElement('article');
-cartao.className = 'cartao';
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
-cartao.innerHTML = `
+    cartao.innerHTML = `
 <div class="cartao__conteudo">
-                    <h3>${desenhos animados}</h3>
+                    <h3>${desenhos animados}</h3 >
                     <div class="cartao__conteudo__pergunta">
                         <p>${Qual é o nome do cãozinho companheiro do Mickey Mouse}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
                         <p>${pluto}</p>
                     </div>
-                </div>
-     `
+                </div >
+    `
 container.appendChild(cartao);
-    }
+ }
